41c318246032914104839e8861c4b4da14deef5c
NathanCrowley <crowley2305@gmail.com>
2022-02-08 10:32:04 +0000

    added report writing tips PDF's found on https://project.cs.ucc.ie/

--- Research/VennDiagram_research/VennDiagramPython/test_features.py
+++ Research/VennDiagram_research/VennDiagramPython/test_features.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
 from matplotlib_venn import venn3, venn3_circles, venn3_unweighted
+
 #Titles
 '''title = "Venn Diagram"'''
 weighted_title = "- weighted "
@@ -16,28 +17,24 @@ two_sets_title = " - 2 sets "
 - the api will know if its a 2 or 3 set diagram based on the labels inputted.
 '''
 ## ------------------------------------------------------------------------------
-'''
-My notes:
-	- for some reason the if statements are all running and the plt.show() shows all the venn diagrams and so they overlap and look terrible.
-'''
 
 class drawVenn:
-	def __init__(self,subsets,labels):
+	def __init__(self,subsets,labels,unweighted=False):
 		self.subsets = subsets
 		self.labels = labels
 		self.number_of_sets = len(labels)
-		self.unweighted = True
+		self.unweighted = unweighted
 		self.title = "Venn Diagram"
 		if self.number_of_sets == 3: # 3 set Venn diagram
 			if self.unweighted == False:
-				self.venn_diagram_3 = venn3(subsets=self.subsets, set_labels=self.labels) 
+				venn3(subsets=self.subsets, set_labels=self.labels) 
 			elif self.unweighted == True:
-				self.venn_diagram_3_unweighted = venn3_unweighted(subsets=self.subsets, set_labels=self.labels)
-		else: # 2 set Venn diagram
-			if self.unweighted == 0:
-				self.venn_diagram_2 = venn2(subsets=self.subsets, set_labels=self.labels)
-			elif self.unweighted == 1:
-				self.venn_diagram_2_unweighted = venn2_unweighted(subsets=self.subsets, set_labels=self.labels)
+				venn3_unweighted(subsets=self.subsets, set_labels=self.labels)
+		elif self.number_of_sets == 2: # 2 set Venn diagram
+			if self.unweighted == False:
+				venn2(subsets=self.subsets, set_labels=self.labels)
+			elif self.unweighted == True:
+				venn2_unweighted(subsets=self.subsets, set_labels=self.labels)
 		
 		
 	def __str__(self):
@@ -56,39 +53,15 @@ class drawVenn:
 				title += unweighted_title
 		plt.title(title)
 		plt.show()
-
-#-------------------------------------------------------------------------------------------------------------------------------------------------	
-
-'''class drawVenn:
-	def __init__(self, subsets, labels):
-		self.subsets = subsets
-		self.labels = labels
-		self.number_of_sets = len(labels)
-		# use the length of the labes to know if its a 2 or 3 set ven diagram
-	
-	def __str__(self):
-		if self.number_of_sets == 3:
-			venn3_unweighted(subsets=self.subsets, set_labels=self.labels)
-			plt.title("Three set Venn Diagram")
-		else:
-			venn2_unweighted(subsets=self.subsets, set_labels=self.labels)
-			plt.title("Two set Venn Diagram")
-		plt.show()
 		
 	# Getters and setters
-	def set_color():
+	def set_color(self):
 		pass
 		
-	def set_labels():
+	def set_labels(self):
 		pass
 		
-	def set_circles_design():
-		pass'''
-		
-		
-
-#--------------Test the Class---------------------------
-
-
-#-------------------------------------------------------
+	def set_circles_design(self):
+		pass'
+#-------------------------------------------------------------------------------------------------------------------------------------------------	
 
--- Research/VennDiagram_research/VennDiagramPython/test_import_library.py
+++ Research/VennDiagram_research/VennDiagramPython/test_import_library.py
@@ -1,14 +1,37 @@
 from test_features import drawVenn
 
 # V1
-set1 = set([1,2])
-set2 = set([4,5])
+v1_set1 = set([1,2,3])
+v1_set2 = set([3,4,5])
 
 # V2
+v2_set1 = set([1,11,5])
+v2_set2 = set([2,22,5])
+v2_set3 = set([3,33,5])
 
-v1 = drawVenn([set1,set2], ('A','B'))
-#v2 = drawVenn((20,10,12,10,9,4,3), ('A','B','C'))
+## Only really have once instance of the venn diagram at a time
 
+'''
+Expect a 2 set Venn Diagram - weighted
+>> v1 = drawVenn([v1_set1,v1_set2], ('A','B'))
+'''
 
-print("V1",v1)
-#print("V2",v2)
+'''
+Excpet a 2 set Venn Diagram - unweighted
+>> v1 = drawVenn([v1_set1,v1_set2], ('A','B'), True)
+'''
+
+'''
+Expect a 3 set VD - weighted
+>> v1 = drawVenn([v2_set1,v2_set2,v2_set3],('A','B','C'))
+'''
+
+'''
+Expect a 3 set VD - unweighted
+v1 = drawVenn([v2_set1,v2_set2,v2_set3],('A','B','C'),True)
+'''
+
+#------------------------------------------------------------------
+v1 = drawVenn([v1_set1,v1_set2], ('A','B'))
+print()
+
